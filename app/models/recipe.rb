d403ebd41c86d1099d74d050f295822887609593
Gregoire Courtois <courtoisgregoire@gmail.com>
2023-10-14 14:25:51 +0200

    Add new method to search recipes

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -19,4 +19,14 @@ class Recipe < ApplicationRecord
   def fill_flat_ingredients
     self.flat_ingredients = ingredients
   end
+
+  def self.iterate_on_ingredients(ingredients, number)
+    ingredients_list = ingredients.split(' ')
+    list_to_query = ingredients_list.combination(ingredients_list.length - 1).to_a
+    hash_count = {}
+    list_to_query.each do |ingredients|
+      hash_count[ingredients] = Recipe.search_by_ingredients(ingredients).count
+    end
+    Recipe.search_by_ingredients(hash_count.key(hash_count.values.max)).first(number)
+  end
 end
